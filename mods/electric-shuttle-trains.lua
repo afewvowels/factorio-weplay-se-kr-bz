4992812c22f2cb237696d332627cb30d6b1cab93
Keith B. Smith <32456218+afewvowels@users.noreply.github.com>
2023-10-20 21:55:38 -0400

    1.0.18a

--- mods/electric-shuttle-trains.lua
+++ mods/electric-shuttle-trains.lua
@@ -1,5 +1,5 @@
 -- Modifications to Electric Shuttle Trains mod to increase electric shuttle fuel capacity beyond 2MW
 
-data.raw["electric-energy-interface"]["electric-shuttle-hidden-receiver-over-stopped-locomotive"].energy_source.buffer_capacity = "10MW"
-data.raw["electric-energy-interface"]["electric-shuttle-hidden-receiver-over-stopped-locomotive"].energy_source.input_flow_limit = "10MW"
-data.raw["item"]["electric-shuttle-hidden-fuel"].fuel_value = "200MJ"
+data.raw["electric-energy-interface"]["electric-shuttle-hidden-receiver-over-stopped-locomotive"].energy_source.buffer_capacity = "100MW"
+data.raw["electric-energy-interface"]["electric-shuttle-hidden-receiver-over-stopped-locomotive"].energy_source.input_flow_limit = "100MW"
+data.raw["item"]["electric-shuttle-hidden-fuel"].fuel_value = "800MJ"
